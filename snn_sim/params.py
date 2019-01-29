504abe7b966f423d26d946cc10d0b5f5bc02905e
Samuel Brown <sbrow109@vols.utk.edu>
2019-01-29 16:33:42 -0500

    Added config file and rudimentary parsing functionality in snn_sim/params.py, copied component classes to snn_sim/snn_components.py

---
+++ snn_sim/params.py
@@ -0,0 +1,51 @@
+# FOR NOW FORMAT AS SCRIPT, NEED TO REFACTOR AS PARAMS CLASS
+
+import configparser
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('params.ini')
+
+# Dict-like structures for parameters from ini file
+gp = config['general_params']
+mp = config['memristance_params']
+np = config['neuron_params']
+ss = config['simulator_settings']
+
+# Set up circuit operating voltages
+VDD = float(gp['VDD'])
+VSS = float(gp['VSS'])
+Vt = VDD - VSS
+
+# Generate all necessary memristance parameters
+HRS = float(mp['HRS'])
+LRS = float(mp['LRS'])
+tswp = float(mp['tswp'])
+tswn = float(mp['tswn'])
+Vthp = float(mp['Vthp'])
+Vthn = float(mp['Vthn'])
+delT = float(mp['delT'])
+
+# Standard deviation multipliers (percent of mean)
+_hrs = float(mp['m_HRS'])
+_lrs = float(mp['m_LRS'])
+_tswn = float(mp['m_tswn'])
+_tswp = float(mp['m_tswp'])
+_Vthn = float(mp['m_Vthn'])
+_Vthp = float(mp['m_Vthp'])
+
+# Statistical memristor parameters
+mu_hrs, sigma_hrs = HRS, HRS*_hrs
+mu_lrs, sigma_lrs = LRS, LRS*_lrs
+mu_tswn, sigma_tswn = tswn, tswn*_tswn
+mu_tswp, sigma_tswp = tswp, tswp*_tswp
+mu_vtn, sigma_vtn = Vthn, Vthn*_Vthn # Is this right?
+mu_vtp, sigma_vtp = Vthp, Vthp*_Vthp # Is this right?
+
+# Memristor increase and decrease intervals
+M_dec = (HRS-LRS)*(delT/tswp)*(Vt/Vthp)
+M_inc = (HRS-LRS)*(delT/tswn)*(Vt/Vthn)
+
+# Print for verification
+print(M_dec)
+print(M_inc)
---
+++ snn_sim/snn_components.py
@@ -0,0 +1,228 @@
+import params.py
+
+##########################################################
+#                                                        #
+#  Definition of Neuron Class                            #
+#  Neuron Membrane potential and firing events are       #
+#  initialized as empty arrays while threshold and       # 
+#  refractory period is assigned from the network file.  #
+#  Each neuron is also assigned a name given in network. #
+#  Synapses connected to the input of the neuron are also#
+#  listed.                                               #
+#                                                        #
+########################################################## 
+
+class Neuron:
+    def __init__(self, vmem=0, fire=0, vth=0, rf=0, Name=''):
+        self.vmem = np.zeros(n)
+        self.fire = np.zeros(n)
+
+        # Define the starting capacitance of integrator
+        #self.cap = cap
+        self.vth = vth - 1
+        self.rf = rf
+        self.Name = Name
+
+        self.vmem[0] = vmem
+        self.input_connections = []
+        
+    def accum1(self,clk,g):
+        if 1.0 not in self.fire[max(0,clk+1-self.rf):clk+1]:
+            self.vmem[clk:] = self.vmem[clk-1] - 0.6*g[clk-1]*tper/cap
+            if self.vmem[clk] < self.vth:
+                self.fire[clk+1] = 1
+                self.vmem[clk+1:] = 0
+                
+    def accum(self,clk,S):
+        if len(S) == 1:
+            if S[0].N1.fire[max(0,clk-S[0].d)] == 1:
+                self.accum1(clk,S[0].G)
+        elif len(S) == 2:
+            if S[0].N1.fire[max(0,clk-S[0].d)] == 1:
+                self.accum1(clk,S[0].G)
+            if S[1].N1.fire[max(0,clk-S[1].d)] == 1:
+                self.accum1(clk,S[1].G)
+                
+    def __str__(self):
+#        return self.Name +' Vmem = ' + str(self.vmem) + '\n' + 'Vf   = ' + str(self.fire)
+        return self.Name + ' Vf = ' + str(self.fire)
+
+
+##########################################################
+#                                                        #
+#  Definition of pseudo input neuron Class               #
+#  Provides input to the input synapses of real input    #
+#  neurons                                               #
+#                                                        #
+########################################################## 
+    
+class inNeu:
+    def __init__(self, fire, Name):
+        self.fire = fire
+        self.Name = Name
+        
+    def __str__(self):
+        return self.Name + ' Vf = ' + str(self.fire)
+
+
+##########################################################
+#                                                        #
+#  Definition of Memristor Class                         #
+#  Initializes the memristive device with process        #
+#  variation parameters.                                 # 
+#  Also models the change in memristance                 #
+#                                                        #
+##########################################################
+        
+    
+class Memristor:
+    def __init__(self,Name):
+        self.HRS = np.random.normal(mu_hrs, sigma_hrs)
+        self.LRS = np.random.normal(mu_lrs, sigma_lrs)
+        self.VtN = np.random.normal(mu_vtn, sigma_vtn)
+        self.VtP = np.random.normal(mu_vtp, sigma_vtp)
+        self.tswN = np.random.normal(mu_tswn, sigma_tswn)
+        self.tswP = np.random.normal(mu_tswp, sigma_tswp)
+        self.Name = Name
+        
+    def __str__(self):
+        return self.Name + '\n HRS = ' + str(self.HRS/1000) +' k\u2126' \
+                         + '\n LRS = ' + str(self.LRS/1000) +' k\u2126' \
+                         + '\n VtN = ' + str(self.VtN)  +' V'\
+                         + '\n VtP = ' + str(self.VtP)  +' V'\
+                         + '\n tswN = ' + str(self.tswN*1e6)  +' \u03bcs'\
+                         + '\n tswP = ' + str(self.tswP*1e9) +' ns'
+    
+    def Mdec(self):
+        return (self.HRS-self.LRS)*(delT/self.tswP)*(Vt/self.VtP)
+    
+    def Minc(self):
+        return (self.HRS-self.LRS)*(delT/self.tswN)*(Vt/self.VtN)
+
+
+##########################################################
+#                                                        #
+#  Definition of Synapse Class                           #
+#  Memristor Mp, Mn and synaptic delay is assigned from  #
+#  network file. Pre-Neuron, N1 and post neuron, N2      # 
+#  connections are also assigned.                        #
+#                                                        #
+########################################################## 
+
+    
+class Synapse:
+    def __init__(self, Mp=25000, Mn=25000, d=0, N1=Neuron(0,0,0,0,'Vpre'), N2=Neuron(0,0,0,0,'Vpost')):
+        self.Mp = np.ones(n) * Mp
+        self.Mn = np.ones(n) * Mn
+        G = 1/Mp - 1/Mn
+        self.G = np.ones(n) * G
+        self.d = d
+        self.N1 = N1
+        self.N2 = N2
+        self.Memp = Memristor('Mp_'+ self.N1.Name + '-->' + self.N2.Name)
+        self.Memn = Memristor('Mn_'+ self.N1.Name + '-->' + self.N2.Name)
+    
+
+    def __str__(self):
+        return 'Geff = ' + str(self.G/Gm) + ' Connection ' + self.N1.Name+ '-->' + self.N2.Name
+    
+    def LTP1(self,clk):
+        self.Mp[clk] -= self.Memp.Mdec()
+        self.Mn[clk] += self.Memn.Minc()
+    
+    def LTD1(self,clk):
+        self.Mp[clk] += self.Memp.Minc()
+        self.Mn[clk] -= self.Memn.Mdec()
+    
+#    def LTP1(self,clk):
+#        self.Mp[clk] -= M_dec
+#        self.Mn[clk] += M_inc
+        
+    def LTP2(self,clk):
+        self.Mp[clk] -= 2*M_dec
+        self.Mn[clk] += 2*M_inc    
+    
+    def LTP3(self,clk):
+        self.Mp[clk] -= 3*M_dec
+        self.Mn[clk] += 3*M_inc
+        
+    def LTP4(self,clk):
+        self.Mp[clk] -= 4*M_dec
+        self.Mn[clk] += 4*M_inc 
+        
+    def LTP5(self,clk):
+        self.Mp[clk] -= 5*M_dec
+        self.Mn[clk] += 5*M_inc
+        
+#    def LTD1(self,clk):
+#        self.Mp[clk] += M_inc
+#        self.Mn[clk] -= M_dec
+        
+    def LTD2(self,clk):
+        self.Mp[clk] += 2*M_inc
+        self.Mn[clk] -= 2*M_dec
+
+    def LTD3(self,clk):
+        self.Mp[clk] += 3*M_inc
+        self.Mn[clk] -= 3*M_dec
+
+    def LTD4(self,clk):
+        self.Mp[clk] += 4*M_inc
+        self.Mn[clk] -= 4*M_dec
+
+    def LTD5(self,clk):
+        self.Mp[clk] += 5*M_inc
+        self.Mn[clk] -= 5*M_dec        
+        
+    def STDP(self,clk):
+        self.Mp[clk] = self.Mp[clk-1]
+        self.Mn[clk] = self.Mn[clk-1]
+        
+        if STDP_cycle == 1:
+            if self.N1.fire[clk-self.d-1] == 1 and self.N2.fire[clk] == 1:
+                self.LTP1(clk)
+            elif self.N1.fire[clk-self.d] == 1 and self.N2.fire[clk] == 1:
+                self.LTD1(clk)
+        elif STDP_cycle == 2:
+            if self.N1.fire[clk-self.d-2] == 1 and self.N2.fire[clk] == 1:
+                self.LTP1(clk)
+            elif self.N1.fire[clk-self.d-1] == 1 and self.N2.fire[clk] == 1:
+                self.LTP2(clk)                
+            elif self.N1.fire[clk-self.d] == 1 and self.N2.fire[clk] == 1:
+                self.LTD2(clk)
+            elif self.N1.fire[clk-self.d] == 1 and self.N2.fire[clk-1] == 1:
+                self.LTD1(clk)
+                
+        if self.Mp[clk] < LRS:
+            self.Mp[clk] = LRS
+        if self.Mn[clk] < LRS:
+            self.Mn[clk] = LRS
+        if self.Mp[clk] > HRS:
+            self.Mp[clk] = HRS
+        if self.Mn[clk] > HRS:
+            self.Mn[clk] = HRS
+
+        self.G[clk] = 1/self.Mp[clk] - 1/self.Mn[clk]
+
+##########################################################
+#                                                        #
+#  Definition of input Synapse Class                     #
+#  Memristor Mp, Mn and synaptic delay is assigned from  #
+#  network file. Also takes the input firing information # 
+#                                                        #
+########################################################## 
+
+        
+class inSyn:
+    def __init__(self, Mp=LRS, Mn=HRS, d=0, N1=inNeu([],'')):
+        self.Mp = Mp
+        self.Mn = Mn
+        G = 1/Mp - 1/Mn
+        self.G = np.ones(n) * G
+        self.d = d
+        self.N1 = N1
+        
+    def __str__(self):
+        return 'Geff = ' + str(self.G/Gm) + ' Connection ' + self.N1.Name
+
+
